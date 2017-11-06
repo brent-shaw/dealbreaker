33caf41071b76b374875ebfedf7c6ca47bd2d3be
brent-shaw <shaw@live.co.za>
2017-11-06 21:38:29 +0200

    Housekeeping and update
    Moved from hacky script to slightly more formal functions. Also moved to PhantomJS for headless capture.

--- archiveDeals.py
+++ archiveDeals.py
@@ -1,43 +1,82 @@
 #!/usr/bin/env python
-from contextlib import closing
-from selenium.webdriver import Firefox
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup, SoupStrainer
 import pickle
 import datetime
 
-address = "url_here"
+deals = []
 
-print "Fetching sauce"
-try:
-    with closing(Firefox()) as browser:
-        browser.get(address)
-        WebDriverWait(browser, timeout=10).until(
-            lambda x: x.find_element_by_link_text("Daily Deals"))
-        page_source = browser.page_source
+def getHTML(url, title):
+    browser = webdriver.PhantomJS('phantomjs')
+    browser.get(url)
+    WebDriverWait(browser, timeout=20).until(
+        lambda x: x.find_element_by_link_text(title))
+    return browser.page_source
 
-    print "Making soup"
-    soup = BeautifulSoup(page_source, 'lxml')
+def getDeals(html):
+    soup = BeautifulSoup(html, 'lxml')
     deals = []
+    for url in soup.find_all('div', class_=r'daily-deal-item group active'):
+        try:
+            pid = url.a.get('href').encode('ascii', 'ignore').decode('ascii').rsplit('/', 1)[-1]
+            name = url.find('div', class_='deal-info').h3.text.encode('ascii', 'ignore').decode('ascii')
+            link = url.a.get('href').encode('ascii', 'ignore').decode('ascii')
+            dp = url.find('div', class_='deal-info').div.p.text.encode('ascii', 'ignore').decode('ascii')
+            try:
+                lp = url.find('div', class_='deal-info').div.div.p.text.encode('ascii', 'ignore').decode('ascii')
+            except:
+                lp = 0
 
-    print "Straining"
-    for url in soup.find_all('div', class_='daily-deal-item group active'):
-        pid = url.a.get('href').encode('ascii', 'ignore').decode('ascii').rsplit('/', 1)[-1]
-        name = url.find('div', class_='deal-info').h3.text.encode('ascii', 'ignore').decode('ascii')
-        link = url.a.get('href').encode('ascii', 'ignore').decode('ascii')
-        dp = url.find('div', class_='deal-info').div.p.text.encode('ascii', 'ignore').decode('ascii')
-        lp = url.find('div', class_='deal-info').div.div.p.text.encode('ascii', 'ignore').decode('ascii')
+            deal = (pid, name, link, dp, lp)
+            deals.append(deal)
 
-        deal = (pid, name, link, dp, lp)
-        deals.append(deal)
+        except Exception as e:
+            print(e)
+    return deals
 
-    print "Pickling"
+def moreDeals(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    paginator = soup.find('div', class_='filter-paginator pagination')
+
+    n = None
+
+    for url in paginator.find_all('a'):
+        try:
+            if url.get('rel') == ['next']:
+                n = url.get('href')
+        except:
+            pass
+
+    return n
+
+def scrapePage(address, title):
+    try:
+        page_source = getHTML(address, title)
+
+        deals.extend(getDeals(page_source))
+
+        n = moreDeals(page_source)
+
+        if (n != None):
+            address = 'https://'+address.split('/')[2]+n
+            scrapePage(address, title)
+        else:
+            pass
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    title = ""
+    address = ""
+
+    scrapePage(address, title)
+
     try:
         with open(str(datetime.datetime.now()), 'wb') as f:
             pickle.dump(deals, f)
-            print "Soup's up"
-    except:
-        print "Jar broke"
+            print "Got "+str(len(deals))+" pickels!"
 
-except:
-    print "Shop's closed"
+    except Exception as e:
+        print(e)
